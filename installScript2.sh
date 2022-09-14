a3844ee795cda2ebd9f2a7487aae1e428fd85a10
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-14 15:48:22 -0400

    Update installScript2.sh

--- installScript2.sh
+++ installScript2.sh
@@ -10,7 +10,10 @@ sudo chmod +x ~/tak-server/scripts/setup.sh
 
 sudo cp ~/tak-zip/* ~/tak-server
 
-echo "Running install script..."
+echo "YOU ARE NOW READY TO RUN THE DOCKER TAK INSTALL SCRIPT"
+echo " "
 echo "=====> See https://atakhq.com/en/tak-server/setup-guide for detailed guide on what to expect during this stage"
+echo " "
 cd ~/tak-server
-sudo ./scripts/setup.sh
+echo "Run the following command to start the script"
+echo "./scripts/setup.sh"
