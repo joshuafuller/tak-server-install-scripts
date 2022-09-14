2d6eddc0982a88c00be2e4a668308d752abee970
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-14 14:05:49 -0400

    Update installScript1.sh

--- installScript1.sh
+++ installScript1.sh
@@ -9,7 +9,7 @@ passwd tak
 
 #Add sudo rights, install deps
 gpasswd -a tak wheel
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo yum update -y
 sudo yum install unzip git nano openssl keytool-maven-plugin docker wget -y
