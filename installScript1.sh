32f3728c2ce153c8ea09efca56ab0946da952617
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-14 15:34:00 -0400

    Update installScript1.sh

--- installScript1.sh
+++ installScript1.sh
@@ -8,7 +8,7 @@ passwd tak
 #user prompt for pw
 
 #Add sudo rights, install deps
-gpasswd -a tak wheel
+sudo gpasswd -a tak wheel
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo yum -y install epel-release
@@ -21,6 +21,7 @@ sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker tak
 sudo mkdir /home/tak/tak-zip
+sudo chown tak:tak /home/tak/tak-zip
 #copy the install script 2 into tak user folder so we can run it later
 sudo cp /root/tak-server-install-scripts/installScript2.sh /home/tak
 sudo chown tak:tak /home/tak/installScript2.sh
