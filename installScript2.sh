0d2bcab6cc663df4a705597a47a34ca864d90452
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-14 15:53:56 -0400

    Update installScript2.sh

--- installScript2.sh
+++ installScript2.sh
@@ -5,7 +5,7 @@ echo "This script will begin the install of TAK Server"
 read -p "Press any key to begin ..."
 
 echo "Cloning CloudRF Github Docker Install Project..."
-sudo git clone https://github.com/Cloud-RF/tak-server.git
+git clone https://github.com/Cloud-RF/tak-server.git
 sudo chmod +x ~/tak-server/scripts/setup.sh
 
 sudo cp ~/tak-zip/* ~/tak-server
