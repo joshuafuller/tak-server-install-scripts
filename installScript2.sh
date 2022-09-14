456006a8daf054c4d0e048c0b4a9287be238dfef
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-14 13:20:35 -0400

    Update installScript2.sh

--- installScript2.sh
+++ installScript2.sh
@@ -1,22 +1,16 @@
-echo "***********************************************************************"
-echo "***********************************************************************"
-echo " "
 echo "MAKE SURE YOU ARE RUNNING THIS UNDER TAK USER WITH SU PRIVS: 'su - tak'"
 echo " "
-echo "***********************************************************************"
-echo "***********************************************************************"
 
 echo "This script will begin the install of TAK Server"
-echo " "
 read -p "Press any key to begin ..."
 
 echo "Cloning CloudRF Github Docker Install Project..."
-git clone https://github.com/Cloud-RF/tak-server.git
-chmod +x ~/tak-server/scripts/setup.sh
+sudo git clone https://github.com/Cloud-RF/tak-server.git
+sudo chmod +x ~/tak-server/scripts/setup.sh
 
 sudo cp ~/tak-zip/* ~/tak-server
 
 echo "Running install script..."
 echo "=====> See https://atakhq.com/en/tak-server/setup-guide for detailed guide on what to expect during this stage"
 cd ~/tak-server
-./scripts/setup.sh
+sudo ./scripts/setup.sh
