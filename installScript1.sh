ab218bdafb039340506811712aff8cd18d4e1cc3
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-14 15:55:47 -0400

    Update installScript1.sh

--- installScript1.sh
+++ installScript1.sh
@@ -1,14 +1,7 @@
 echo "Run this script to begin the install process for TAK Server, it will take a while so please be patient."
 read -p "Press any key to begin ..."
 
-#setup non-root user
-echo "Creating non-root user 'tak'..."
-adduser tak
-passwd tak
-#user prompt for pw
 
-#Add sudo rights, install deps
-sudo gpasswd -a tak wheel
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo yum -y install epel-release
