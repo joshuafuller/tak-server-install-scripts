5340cc2a016d6da32f04aaff34b769fa5da82c11
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-19 16:26:25 -0400

    Update installScript1.sh

--- installScript1.sh
+++ installScript1.sh
@@ -1,18 +1,33 @@
 echo "Run this script to begin the install process for TAK Server, it will take a while so please be patient."
 read -p "Press any key to begin ..."
 
+adduser tak
+passwd tak
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+usermod -aG sudo tak
+sudo apt-get update -y
+
+#Install Deps
+sudo apt-get install unzip wget git nano openssl net-tools -y
+
+#Install Docker
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt-get update
+sudo apt-get -y install docker-ce
+
+#Install Docker-Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo yum -y install epel-release
-sudo yum repolist
-sudo yum install dpkg-devel dpkg-dev -y
-sudo yum update -y
-sudo yum install unzip wget git nano openssl docker net-tools -y
+
+#Start docker, run at system startup
 sudo systemctl start docker
 sudo systemctl enable docker
+
 sudo groupadd docker
 sudo usermod -aG docker tak
+
 sudo mkdir /home/tak/tak-zip
 sudo chown tak:tak /home/tak/tak-zip
 #copy the install script 2 into tak user folder so we can run it later
