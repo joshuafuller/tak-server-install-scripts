32975260dbf766625cd1b86571a0017b44809510
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-09-27 10:10:25 -0400

    Update localInstallScript.sh

--- localInstallScript.sh
+++ localInstallScript.sh
@@ -12,16 +12,39 @@ sudo apt-get update -y
 #Install Deps
 sudo apt-get install unzip wget git nano openssl net-tools -y
 
-#Install Docker
-sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-sudo apt-get update
-sudo apt-get -y install docker-ce
+#Determine if amd or arm build and install appropriate Docker build
+ARCH=$(dpkg --print-architecture)
+
+if [ $ARCH == "arm64" ];
+then
+	echo "Building for arm64 (RPI/BPI)..."
+  #Install Docker
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update
+  sudo apt-get -y install docker-ce
+  echo "*** DOCKER INSTALLED ***"
+fi
+
+if [ $ARCH == "amd64" ];
+then
+	echo "Building for amd64..."
+  #Install Docker
+  sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+  sudo apt-get update
+  sudo apt-get -y install docker-ce
+  echo "*** DOCKER INSTALLED ***"
+fi
+
 
 #Install Docker-Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.10.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+echo "*** DOCKER COMPOSE INSTALLED ***"
+docker-compose --version
 
 #Start docker, run at system startup
 sudo systemctl start docker
