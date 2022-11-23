bb752c2dc086c5868c2a80b0e9a9ac1ffd9b66b9
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-11-23 15:18:58 -0500

    Update installScript2.sh

--- installScript2.sh
+++ installScript2.sh
@@ -26,21 +26,21 @@ read transferoption
                         echo "*****************************************"
                         echo ""
                         #Create service file
-                        sudo tee /etc/systemd/system/tak-server-docker.service >/dev/null << EOF
-                        [Unit]
-                        Description=TAK Server (Docker)
-                        Requires=docker.service
-                        After=docker.service
-                        [Service]
-                        Type=oneshot
-                        RemainAfterExit=yes
-                        WorkingDirectory=/home/tak/tak-server
-                        ExecStart=/usr/local/bin/docker-compose up -d
-                        ExecStop=/usr/local/bin/docker-compose down
-                        TimeoutStartSec=0
-                        [Install]
-                        WantedBy=multi-user.target
-                        EOF
+sudo tee /etc/systemd/system/tak-server-docker.service >/dev/null << EOF
+[Unit]
+Description=TAK Server (Docker)
+Requires=docker.service
+After=docker.service
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+WorkingDirectory=/home/tak/tak-server
+ExecStart=/usr/local/bin/docker-compose up -d
+ExecStop=/usr/local/bin/docker-compose down
+TimeoutStartSec=0
+[Install]
+WantedBy=multi-user.target
+EOF
 
                         #Enable it to run on reboots
                         sudo systemctl enable tak-server-docker
