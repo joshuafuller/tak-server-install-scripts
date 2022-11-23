c01431d2af29983669a716b2d0cf1b4d027e2683
ATAK HQ <113393493+atakhq@users.noreply.github.com>
2022-11-23 14:55:02 -0500

    Update installScript2.sh

--- installScript2.sh
+++ installScript2.sh
@@ -11,6 +11,56 @@ git clone https://github.com/Cloud-RF/tak-server.git
 echo "Done, updating script permissions"
 sudo chmod +x ~/tak-server/scripts/setup.sh
 
+
+#Handle auto-start setup
+while true; do
+echo "Do you want to enable auto-start on server reboot? (systemd service)"
+echo "Y: Yes"
+echo "N: No"
+echo ""
+read transferoption
+        case $transferoption in
+                [Yy]* )
+                        echo "*****************************************"
+                        echo "Setting up auto-start for TAK-Server"
+                        echo "*****************************************"
+                        echo ""
+                        #Create service file
+                        sudo tee /etc/systemd/system/tak-server-docker.service >/dev/null << EOF
+                        [Unit]
+                        Description=TAK Server (Docker)
+                        Requires=docker.service
+                        After=docker.service
+                        [Service]
+                        Type=oneshot
+                        RemainAfterExit=yes
+                        WorkingDirectory=/home/tak/tak-server
+                        ExecStart=/usr/local/bin/docker-compose up -d
+                        ExecStop=/usr/local/bin/docker-compose down
+                        TimeoutStartSec=0
+                        [Install]
+                        WantedBy=multi-user.target
+                        EOF
+
+                        #Enable it to run on reboots
+                        sudo systemctl enable tak-server-docker
+
+                        echo "Setup Complete:"
+                        echo "TAK Server should now auto start on server reboots" 
+                        break
+                ;;
+                [Nn]* )
+                        echo "*****************************************"
+                        echo "SKIPPING SYSTEMD AUTO-START SETUP"
+                        echo "*****************************************"
+                        break
+                ;;
+                * ) echo "You must select an option to continue";;
+        esac
+done
+
+
+
 #Handle transfer methods
 while true; do
 echo "How do you want to transfer your TAK Server ZIP File?"
